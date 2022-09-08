3d3208fbb1cb9bb570fb01852a1350cec259120e
张坤 <929794551@qq.com>
2022-09-08 13:48:00 +0800

    onInstalled

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 2. 打开扩展程序管理页面。
 3. 点击右上角开启开发者模式。
 4. 点击 加载已解压的扩展程序。选择第 1 步的文件夹。
+
+![](https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/095dc0e947fe4638832d3064b4eac1b8~tplv-k3u1fbpfcp-watermark.image?)
--- background.js
+++ background.js
@@ -1,37 +1,39 @@
-chrome.contextMenus.create({
-	title: '搜索',
-	id: 'search',
-	contexts: ['all'],
-});
-chrome.contextMenus.create({
-	title: '百度',
-	parentId: 'search',
-	id: 'baidu',
-	contexts: ['all'],
-});
-chrome.contextMenus.create({
-	title: '必应',
-	parentId: 'search',
-	id: 'bing',
-	contexts: ['all'],
-});
-chrome.contextMenus.create({
-	title: '360',
-	parentId: 'search',
-	id: '360',
-	contexts: ['all'],
-});
-chrome.contextMenus.create({
-	title: '搜狗',
-	parentId: 'search',
-	id: 'sogou',
-	contexts: ['all'],
-});
-chrome.contextMenus.create({
-	title: '谷歌',
-	parentId: 'search',
-	id: 'google',
-	contexts: ['all'],
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+  	title: '搜索',
+  	id: 'search',
+  	contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+  	title: '百度',
+  	parentId: 'search',
+  	id: 'baidu',
+  	contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+  	title: '必应',
+  	parentId: 'search',
+  	id: 'bing',
+  	contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+  	title: '360',
+  	parentId: 'search',
+  	id: '360',
+  	contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+  	title: '搜狗',
+  	parentId: 'search',
+  	id: 'sogou',
+  	contexts: ['all'],
+  });
+  chrome.contextMenus.create({
+  	title: '谷歌',
+  	parentId: 'search',
+  	id: 'google',
+  	contexts: ['all'],
+  });
 });
 const sendData = (data) => {
 	chrome.tabs.query({active: true,currentWindow: true}).then(tabs=>{
